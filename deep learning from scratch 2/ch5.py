d2e43de4810c3ad07b7817fb93dc4973858ae5cd
minho-kim <mincco94@gmail.com>
2021-12-06 23:43:55 +0900

    5.3

---
+++ deep learning from scratch 2/ch5.py
@@ -0,0 +1,48 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.13.0
+#   kernelspec:
+#     display_name: cook
+#     language: python
+#     name: cook
+# ---
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class RNN:
+    def __init__(self, Wx, Wh, b):
+        self.params = [Wx, Wh, b]
+        self.grads = [np.zeros_like(Wx), np.zeros_like(Wb), np.zeros_like(b)]
+        self.cache = None
+        
+    def forward(self, x, h_prev):
+        Wx, Wh, b = self.parms
+        t = np.matmul(h_prev, Wh) + np.matmul(x, Wx) + b
+        h_next = np.tanh(t)
+        
+        self.cache = (x, h_prev, h_next)
+        return h_next
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
