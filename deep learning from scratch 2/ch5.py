d7633750700b0d1dabeb9b9156b0497a08585c81
minho-kim <mincco94@gmail.com>
2021-12-10 17:04:46 +0900

    RNN backward

--- deep learning from scratch 2/ch5.py
+++ deep learning from scratch 2/ch5.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # ---
 # jupyter:
 #   jupytext:
@@ -29,9 +30,25 @@ class RNN:
         h_next = np.tanh(t)
         
         self.cache = (x, h_prev, h_next)
+        # 역전파 계산시 사용하는 중간 데이터 담는 곳.
         return h_next
+    
+    def backward(self, dh_next):
+        Wx, Wb, b = self.params
+        x, h_prev, h_next = self.cache
+        
+        dt = dh_next * (1 - h_next ** 2)
+        db = np.sum(dt, axis = 0)
+        dWh = np.matmul(h_prev.T, dt)
+        dh_prev = np.matmul(x.T, dt)
+        dx = np.matmul(dt, Wx.T)
+        
+        self.grads[0][...] = dWx
+        self.grads[1][...] = dWh
+        self.grads[2][...] = db
 
 
+aa = RNN()
 
 
 
