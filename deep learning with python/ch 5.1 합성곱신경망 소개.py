49f48334370e98928bd394cbfa140fd0c74a015b
minho-kim <mincco94@gmail.com>
2022-01-03 23:35:49 +0900

    first Conv2D

---
+++ deep learning with python/ch 5.1 합성곱신경망 소개.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.13.0
+#   kernelspec:
+#     display_name: cook
+#     language: python
+#     name: cook
+# ---
+
+# # 5.1 합성곱 신경망 소개
+
+# +
+# 기본적인 컨브넷
+from keras import layers
+from keras import models
+
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+# -
+
+model.summary()
+
+model.add(layers.Flatten())
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(10, activation='softmax'))
+model.summary()
