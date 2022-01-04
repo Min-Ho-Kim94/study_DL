4d190df69da280365583803296cc728d04423651
minho-kim <mincco94@gmail.com>
2022-01-05 00:15:49 +0900

    conv2D MNIST

--- deep learning with python/ch 5.1 합성곱신경망 소개.py
+++ deep learning with python/ch 5.1 합성곱신경망 소개.py
@@ -35,3 +35,49 @@ model.add(layers.Flatten())
 model.add(layers.Dense(64, activation='relu'))
 model.add(layers.Dense(10, activation='softmax'))
 model.summary()
+
+# +
+# MNIST
+from keras.datasets import mnist
+from tensorflow.keras.utils import to_categorical
+
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+train_images = train_images.reshape((60000, 28, 28, 1))
+train_images = train_images.astype('float32') / 255
+
+test_images = test_images.reshape((10000, 28, 28, 1))
+test_images = test_images.astype('float32') / 255
+
+train_labels = to_categorical(train_labels)
+test_labels = to_categorical(test_labels)
+
+model.compile(optimizer='rmsprop',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+model.fit(train_images, train_labels, epochs=5, batch_size=64)
+# -
+
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+test_acc
+
+
+# ## 5-1 합성곱 연산
+
+# +
+# Dense() : 완전연결층
+# Conv2D() : 합성곱 층 - 지역패턴을 학습
+
+# +
+def generator():
+    i = 0
+    while True:
+        i += 1
+        yield i
+
+for item in generator():
+    print(item)
+    if item > 4:
+        break
+# -
+
+
